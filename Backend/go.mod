ce7d20e7b5c812f1e0775d5c8f1952e7e76d1bfa
prachin007 <prachinnayak07@gmail.com>
2026-02-25 15:22:20 +0530

    Backend Module added.

---
+++ Backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/prachin77/insight-hub
+
+go 1.22.1
---
+++ Backend/main.go
@@ -0,0 +1,13 @@
+package backend
+
+func main(){
+	// Initialize Firebase DB 
+
+	// Get Credentials from .env 
+
+	// Create gin-gonic server 
+
+	// Specify Get Routes 
+
+	// Specify Post routes 
+}
